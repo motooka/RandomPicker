0b9302a6d15ebe9c2d8eee7d411b43a4b7e97a51
Tadahisa MOTOOKA <t.motooooooka@gmail.com>
2021-07-04 19:26:08 +0900

    ローカル開発環境のサーバをDocker上のnginxからWebpackDevServerに切り替え

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,9 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.js',
     },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 20080,
+    }
 };
